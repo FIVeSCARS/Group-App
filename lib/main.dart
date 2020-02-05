3a6c11f56c03e50bc703c80b40c5ea28ae85963e
Talal Al-Lafi <allafi.talal@gmail.com>
2020-02-05 12:55:48 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,63 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Group App",
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text("Group App"),
-        ),
-        body: getListView(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.add),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
 
-Widget getListView() {
-    return Container(
-      width: 380,
-      height: 50,
-      decoration: BoxDecoration(
-          color: Colors.transparent,
-          borderRadius: BorderRadius.circular(40)),
-      child: Column(
-        children: <Widget>[
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-          TextField(
+class _HomePageState extends State<HomePage> {
+  List<String> groups = [];
+  final controller = TextEditingController();
+
+  Widget build(context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text("Groups Page"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: onPressed,
+          child: Icon(Icons.add),
+        ),
+        body: groups.isEmpty
+            ? Center(child: Text('No groups'))
+            : ListView(
+                children: [
+                  ...groups.map(
+                    (group) => Card(
+                        elevation: 3,
+                        child: ListTile(
+                          title: Text(group),
+                          trailing: Icon(Icons.web),
+                          onTap:  () => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SecondPage() )),
+                        )),
+                  )
+                ],
+              ),
+      );
+
+
+
+  void onPressed() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Insert group"),
+        content: Container(
+          width: 250,
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(40)),
+          child: TextField(
+            controller: controller,
             decoration: InputDecoration(
               filled: true,
               fillColor: Colors.white10,
@@ -44,9 +75,112 @@ Widget getListView() {
               hintText: "Group Name",
             ),
           ),
-
+        ),
+        actions: <Widget>[
+          RaisedButton(
+            child: Text("Insert"),
+            color: Colors.green,
+            onPressed: () {
+              setState(() => groups.add(controller.text));
+              controller.clear();
+              Navigator.of(context).maybePop();
+            },
+          ),
+          RaisedButton(
+            child: Text("Exit"),
+            color: Colors.green,
+            onPressed: () {
+              controller.clear();
+              Navigator.of(context).maybePop();
+            },
+          )
         ],
-      )
-  );
+      ),
+    );
+  }
+}
+//
+
+class SecondPage extends StatefulWidget {
+  @override
+  _SecondPageState createState() => _SecondPageState();
 }
 
+class _SecondPageState extends State<SecondPage> {
+  List<String> groups = [];
+  final controller = TextEditingController();
+
+  Widget build(context) =>
+      Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text("Members Page"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: onPressed,
+          child: Icon(Icons.add),
+        ),
+        body: groups.isEmpty
+            ? Center(child: Text('No Members'))
+            : ListView(
+          children: [
+            ...groups.map(
+                  (group) =>
+                  Card(
+                      elevation: 3,
+                      child: ListTile(
+                        title: Text(group),
+                        trailing: Icon(Icons.web),
+                        onLongPress: null,
+                      )),
+            )
+          ],
+        ),
+      );
+  void onPressed() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Insert Members"),
+        content: Container(
+          width: 250,
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(40)),
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white10,
+              suffixIcon: Icon(Icons.group),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              hintText: "Group Name",
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          RaisedButton(
+            child: Text("Insert"),
+            color: Colors.green,
+            onPressed: () {
+              setState(() => groups.add(controller.text));
+              controller.clear();
+              Navigator.of(context).maybePop();
+            },
+          ),
+          RaisedButton(
+            child: Text("Exit"),
+            color: Colors.green,
+            onPressed: () {
+              controller.clear();
+              Navigator.of(context).maybePop();
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
